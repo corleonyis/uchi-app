8c89228f0f84fc90ee6788f6459c575253d67ace
corleonyis <corleonis.nyi531@gmail.com>
2023-08-30 01:38:11 +0900

    NavbarにTextで項目を追加

--- app/src/components/UANavbar.tsx
+++ app/src/components/UANavbar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "@mantine/core";
+import { Navbar, ScrollArea, Divider, Text, Avatar, Flex } from "@mantine/core";
 
 type Props = {
   hidden: boolean
@@ -8,7 +8,24 @@ type Props = {
 export const UANavbar: React.FC<Props> = ({hidden}) => {
   return(
     <Navbar p="md" width={{sm: 200, lg: 300}} hiddenBreakpoint="sm" hidden={hidden}>
-      Application Navbar
+      
+      {/* Navbar Contents */}
+      <Navbar.Section grow component={ScrollArea} mx="-xs" px="xs">
+        <Text>Shopping List</Text>
+        <Text>Stock List</Text>
+        <Text>Calendar</Text>
+        <Text>Settings</Text>
+      </Navbar.Section>
+
+      <Divider my="sm"/>
+      
+      {/* Footer */}
+      <Navbar.Section> 
+        <Flex align="center" gap="md">
+          <Avatar src={null} radius="xl"/>
+          <Text> User Name </Text>
+        </Flex>
+      </Navbar.Section>
     </Navbar>
   )
 }
