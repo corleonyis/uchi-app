c0290db27934a87d73e607e1b9972dcca95c7c33
corleonyis <corleonis.nyi531@gmail.com>
2023-09-24 21:51:57 +0900

    firebaseに関する記述場所を変更

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -6,28 +6,6 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { MantineProvider } from "@mantine/core";
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
