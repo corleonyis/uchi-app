af85b30710012116410f5049679dc4822f19d15e
corleonyis <corleonis.nyi531@gmail.com>
2023-09-30 21:25:58 +0900

    ログイン・ログアウト周りの画面遷移等修正

--- app/src/features/auth/components/Auth.tsx
+++ app/src/features/auth/components/Auth.tsx
@@ -1,6 +1,7 @@
 import {
   User,
   browserLocalPersistence,
+  onAuthStateChanged,
   setPersistence,
   signInWithPopup,
 } from "firebase/auth";
@@ -8,12 +9,10 @@ import { createContext, useContext, useEffect, useState } from "react";
 import auth, { provider } from "../../firebase/firebase";
 import { NavigateFunction } from "react-router-dom";
 
-export type AuthContextType = {
+type AuthContextType = {
   currentUser: User | null
-  // login: () => Promise<void>
   login: (navigate: NavigateFunction) => void
   logout: (navigate: NavigateFunction) => void
-  // debug: () => void
 }
 
 const AuthContext = createContext<AuthContextType>(
@@ -21,27 +20,27 @@ const AuthContext = createContext<AuthContextType>(
     currentUser: null,
     login: () => {},
     logout: () => {}
-    // debug: () => {}
   }
 )
 
-// export const useAuthContext = () => useContext(AuthContext)
-export const useAuthContext = () => {
-  const context = useContext(AuthContext)
-  console.log(context)
-  return context
-}
+export const useAuthContext = () => useContext(AuthContext)
 
 // AuthProvider
 type AuthProviderProps = {
   children: React.ReactNode;
 };
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  // const auth = useFirebaseAuth()
   const [currentUser, setCurrentUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+    return onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+      setLoading(false)
+    })  
+  }, [])
+  
   const login = (navigate: NavigateFunction) => {
-    // auth ? 
     setPersistence(auth, browserLocalPersistence).then(() => {
       signInWithPopup(auth, provider)
         .then((result) => {
@@ -49,23 +48,17 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
           // const credential = GoogleAuthProvider.credentialFromResult(result);
           // const token = credential?.accessToken;
           // The signed-in user info.
-          auth.onAuthStateChanged((user) => {
-            console.log(user)
-            setCurrentUser(user);
-            navigate("/lists/buy")  
-          })
+          navigate("/lists/buy")  
         })
       .catch((error) => {
-        console.log(`signInWithPopUp: ${error}`)
+        console.log(error)
       })
     }).catch((error) => {
       console.log(error)
     })
-    // : console.log(" auth not initialized ")
   }
 
   const logout = (navigate: NavigateFunction) => {
-    console.log(currentUser)
     auth?.signOut()
     navigate("/login")
   }
@@ -76,69 +69,9 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     logout
   };
 
-  useEffect(() => {
-    // Firebase Authのメソッド。ログイン状態が変化すると呼び出される
-    // auth?.onAuthStateChanged((user) => {
-    //   console.log(user)
-    // });
-  }, [currentUser]);
-
   return (
     <AuthContext.Provider value={value}>
-      {children}
+      {!loading && children}
     </AuthContext.Provider>
   );
 }
-
-//
-// export const useAuth = () => {
-//   const [currentUser, setUser] = useState<User | null>(null)
-//   const auth = useFirebaseAuth()
-//   // const navigate = useNavigate()
-
-//   useEffect(() => {
-//     return onAuthStateChanged(auth, (user) => {
-//       if (user) {
-//         console.dir(`login: ${user}`)
-//         // console.dir(`login: ${auth.currentUser}`)
-//         // console.log(`login: ${currentUser}`)
-//         setUser(user)
-//       } else {
-//         console.log(`logout: ${currentUser}`)
-//         setUser(null)
-//       }
-//     })
-//   }, [])
-
-//   const login = (navigate: NavigateFunction) => {
-//     setPersistence(auth, browserLocalPersistence).then(() => {
-//       signInWithPopup(auth, provider)
-//         .then((result) => {
-//           // This gives you a Google Access Token. You can use it to access the Google API.
-//           // const credential = GoogleAuthProvider.credentialFromResult(result);
-//           // const token = credential?.accessToken;
-//           // The signed-in user info.
-//           console.log(`signInWithRedirect: ${result}`)
-//           navigate("/lists/buy")
-//         })
-//       .catch((error) => {
-//         console.log(`signInWithRedirect: ${error}`)
-//       })
-//     }).catch((error) => {
-//       console.log(error)
-//     })
-//   }
-
-//   const logout = () => {
-//     // auth.signOut()
-//     // setUser(null)
-//     // navigate("/login")
-//     console.log(`debug: ${currentUser}`)
-//   }
-
-//   return {
-//     currentUser,
-//     login,
-//     logout,
-//   }
-// }
--- app/src/features/auth/routes/Login.tsx
+++ app/src/features/auth/routes/Login.tsx
@@ -1,32 +1,34 @@
 import { Flex, Title, Button, Center, Stack, Paper } from "@mantine/core";
 import { FcGoogle } from "react-icons/fc";
-// import { useAuth } from "../components/Auth";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useAuthContext } from "../components/Auth";
 
 export const Login: React.FC = () => {
-  // const login = useAuth().login;
-  const { login } = useAuthContext()
+  const { currentUser, login } = useAuthContext()
   const navigate = useNavigate()
+
   return (
-    <Flex gap={"md"} align="center" justify="center" wrap={"wrap"} p={"lg"}>
-      <Paper
-        withBorder
-        radius={"lg"}
-        shadow="md"
-        p={"lg"}
-        style={{ width: 350 }}
-      >
-        <Stack align="center" justify="center">
-          <Title order={3}>ログイン</Title>
-          <Button variant="default" onClick={ () => {login(navigate)}}>
-            <Flex justify={Center} align={Center} gap={"md"}>
-              <FcGoogle />
-              Googleアカウントでログイン
-            </Flex>
-          </Button>
-        </Stack>
-      </Paper>
-    </Flex>
+    currentUser ? 
+      <Navigate to={"/lists/buy"}/>
+    :
+      <Flex gap={"md"} align="center" justify="center" wrap={"wrap"} p={"lg"}>
+        <Paper
+          withBorder
+          radius={"lg"}
+          shadow="md"
+          p={"lg"}
+          style={{ width: 350 }}
+        >
+          <Stack align="center" justify="center">
+            <Title order={3}>ログイン</Title>
+            <Button variant="default" onClick={ () => {login(navigate)}}>
+              <Flex justify={Center} align={Center} gap={"md"}>
+                <FcGoogle />
+                Googleアカウントでログイン
+              </Flex>
+            </Button>
+          </Stack>
+        </Paper>
+      </Flex>
   );
 };
