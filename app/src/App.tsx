07ba296d860674074bfee8d72de5b9ff1194cab1
corleonyis <corleonis.nyi531@gmail.com>
2023-10-01 12:16:08 +0900

    文字フォントを設定

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import { MantineProvider } from "@mantine/core";
 import { AppRouter } from "./routes";
 import { AuthProvider } from "./features/auth/components/Auth";
 
 const App: React.FC = () => {
   return (
-    <MantineProvider>
+    <MantineProvider
+      theme={{fontFamily: "'M PLUS 1p', sans-serif"}}
+      withGlobalStyles
+    >
       <AuthProvider>
         <AppRouter />
       </AuthProvider>
